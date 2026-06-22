3ae397387e1c04567979960bc8f2d2c25e57de02
Your Name <your@gmail.com>
2026-06-22 09:02:50 +0530

    Fix: list view buttons 3-col grid, login float animations, hero stats, clock fix

--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -451,6 +451,20 @@ function renderMedicines(ft = '') {
                         </button>
                     </div>
                 </div>
+                <div class="list-actions">
+                    <button class="edit-btn" onclick="openEditModal('${safeId}')">
+                        <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+                            <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/>
+                            <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/>
+                        </svg>Edit
+                    </button>
+                    <button class="delete-btn" onclick="confirmDelete('${safeId}','${safeName}')">
+                        <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+                            <polyline points="3,6 5,6 21,6"/>
+                            <path d="M19,6v14a2,2,0,0,1-2,2H7a2,2,0,0,1-2-2V6m3,0V4a1,1,0,0,1,1-1h4a1,1,0,0,1,1,1v2"/>
+                        </svg>Remove
+                    </button>
+                </div>
             </div>`);
     });
 
